b9e03141450f1da3952985f02e7a298187b7ec8f
Galo Naranjo <gnaranjo411@gmail.com>
2024-11-26 15:51:05 -0500

    fix(server): update server listening address to allow external connections

--- server/src/server.js
+++ server/src/server.js
@@ -114,6 +114,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
