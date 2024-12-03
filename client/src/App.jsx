665e65486b3aa96b8f64950417c6ff9b0f80a2c7
Galo Naranjo <gnaranjo411@gmail.com>
2024-12-02 20:21:34 -0500

    feat(auth): add LoginPage component and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VideoUpload from "./components/VideoUpload";
 import FeedbackPage from "./components/FeedbackPage";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<VideoUpload />} />
         <Route path="/feedback" element={<FeedbackPage />} />
       </Routes>
